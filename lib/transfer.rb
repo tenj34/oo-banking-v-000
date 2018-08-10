2abdce9346b76a8201de2868753173773bc8ee79
Juan Tenezaca <tenj34@gmail.com>
2018-08-10 18:30:03 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,6 +13,13 @@ class Transfer
   end
 
   def execute_transaction
-
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
   end
 end
