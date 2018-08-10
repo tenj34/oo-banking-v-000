7f2336a81bd05b8cf252c3370299af07c6b4b0ce
Juan Tenezaca <tenj34@gmail.com>
2018-08-10 18:31:45 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,6 +24,10 @@ class Transfer
   end
 
   def reverse_transfer
-    
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
   end
 end
